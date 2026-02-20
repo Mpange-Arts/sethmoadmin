840a106e9e9cd7a63111119e7d6f48c15afd20ce
Justine C Mophat <mpangecreativearts@gmail.com>
2026-02-20 02:52:13 +0200

    api key

--- src/services/heroService.js
+++ src/services/heroService.js
@@ -1,7 +1,7 @@
 // src/services/heroService.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://sethmoserver.onrender.com';
 
 export const getHeroSettings = () => 
   axios.get(`${API_URL}/hero`).then(res => res.data);
